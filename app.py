1316de331d58a3c3f7d474978bac0dd35ec69041
Raghul <raghulrengasamy05@gmail.com>
2026-05-02 20:16:16 +0530

    Add deployment config

--- app.py
+++ app.py
@@ -269,4 +269,4 @@ def opp_to_dict(opp):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
